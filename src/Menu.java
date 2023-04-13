77fc0846855f8a599823f8835cd8e2a3016d213f
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-14 02:43:37 +0430

    add Welcome menu to Menu class

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class Menu {
+    private int chose;
+
+    public int welcomeMenu(Scanner input)
+    {
+        chose=0;
+        System.out.println("1- Sign in ");
+        System.out.println("2- Sign up");
+        System.out.print(">>");
+        chose = input.nextInt();
+        while (chose > 2 || chose <1)
+        {
+            System.out.println("Try again!");
+            System.out.printf(">>");
+            chose = input.nextInt();
+        }
+        return chose;
+    }
 }
