91af8f31e6ed3fdb6f1e5168326d7a071a8c7905
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-14 02:50:25 +0430

    add welcome menu to Utils class

--- src/Utils.java
+++ src/Utils.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Utils {
+    Menu menu = new Menu();
+    Scanner input = new Scanner(System.in);
+    public void start()
+    {
+        while (true)
+        {
+            switch (menu.welcomeMenu(input)) {
+                case 1 -> System.out.println("1");
+                case 2 -> System.out.println("2");
+            }
+        }
+    }
+
 }
