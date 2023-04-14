863f7a3ca7d7af7b710e36458260611a37b03459
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-14 22:56:09 +0430

    add admin simple signIn

--- src/Utils.java
+++ src/Utils.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 public class Utils {
     Menu menu = new Menu();
     Scanner input = new Scanner(System.in);
+    SignIn signin = new SignIn();
     public void start()
     {
         while (true)
         {
             switch (menu.welcomeMenu(input)) {
-                case 1 -> System.out.println("1");
+                case 1 -> signin.signInGet(input);
                 case 2 -> System.out.println("2");
             }
         }
