dc11c3dde882aafee523e2d376bc07d181e7b521
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-15 00:26:07 +0430

    add SignUp class and simple base code

---
+++ src/SignUp.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SignUp {
+    public void siningUpUser(Scanner input, User[] users)
+    {
+        String username;
+
+    }
+    private boolean checkUsername(User[] users)
+    {
+        return true;
+    }
+    private int findIndex(User[] users)
+    {
+        return 0;
+    }
+}
