49251503a57825e5790dc8d7f419618b04ab0787
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-14 22:53:12 +0430

    add admin simple signIn

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,8 +1,35 @@
+import java.nio.file.attribute.UserPrincipalLookupService;
+import java.util.Scanner;
+
 public class SignIn {
-    public void signInGet()
+    public void signInGet(Scanner input)
     {
         String username;
         String password;
-
+        username = input.next();
+        if (adminSignInPassword(username))
+        {
+            password = input.next();
+            if (adminSignInPassword(password))
+            {
+                System.out.println("Welcome admin");
+            }
+            else
+            {
+                System.out.println("password not correct");
+            }
+        }
+        else
+        {
+            System.out.println("username not admin");
+        }
+    }
+    private boolean adminSignInUsername(String userName )
+    {
+        return userName.equals("admin") || userName.equals("Admin") || userName.equals("ADMIN");
+    }
+    private boolean adminSignInPassword(String passWord)
+    {
+        return passWord.equals("admin")||passWord.equals("Admin")||passWord.equals("ADMIN");
     }
 }
