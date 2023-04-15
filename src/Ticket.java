66d8ca7c13258b3848cbadb579e055ea8fe674cb
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-15 23:56:26 +0430

    add Ticket class

---
+++ src/Ticket.java
@@ -0,0 +1,65 @@
+public class Ticket {
+    private String flightId;
+    private String origin;
+    private String destination;
+    private String date;
+    private String time;
+    private int price;
+    private String ticketId;
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+}
