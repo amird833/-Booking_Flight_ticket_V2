416414fdb5307c7242c6a09c370c3c6ad00a46ff
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-15 13:54:16 +0430

    Complete Admin class

--- src/Admin.java
+++ src/Admin.java
@@ -26,7 +26,8 @@ public class Admin {
         {
             switch (adminMenu(input))
             {
-                case 1 :
+                case 1 ://add
+
                     break;
                 case 2 :
                     break;
@@ -41,5 +42,55 @@ public class Admin {
         }
 
     }
+    private void addFlight(Scanner input ,Flight[] flights)
+    {
+        int index;
+        String flightId;
+        System.out.print("FlightId : ");
+        flightId = input.next();
+        while (findExistFlighId(flights,flightId))
+        {
+            System.out.println("This flightId already EXIST");
+            System.out.println("Try again");
+            System.out.printf(">>");
+            flightId = input.next();
+        }
+        index = findNullFlightIndex(flights);
+        flights[index].setFlightId(flightId);
+        System.out.print("\tOrigin : ");
+        flights[index].setOrigin(input.next());
+        System.out.print("\tDestination : ");
+        flights[index].setDestination(input.next());
+        System.out.print("\tDate : ");
+        flights[index].setDate(input.next());
+        System.out.print("\tTime : ");
+        flights[index].setTime(input.next());
+        System.out.print("\tPrice : ");
+        flights[index].setPrice(input.nextInt());
+        System.out.print("\tSeats : ");
+        flights[index].setSeats(input.nextInt());
+    }
+    private int findNullFlightIndex(Flight[] flights)
+    {
+        for (int i = 0; i < flights.length; i++) {
+            if(flights[i]!= null && flights[i].getFlightId() == null)
+                return i;
+        }
+        for (int i = 0; i < flights.length; i++) {
+            if (flights[i] == null) {
+                flights[i] = new Flight();
+                return i;
+            }
+        }
+        return 0;
+    }
+    private boolean findExistFlighId(Flight[] flights,String flightId)
+    {
+        for (int i = 0; i < flights.length; i++) {
+            if (flights[i] != null && flights[i].getFlightId() != null && flights[i].getFlightId().equals(flightId))
+                return true;
 
+        }
+        return false;
+    }
 }
