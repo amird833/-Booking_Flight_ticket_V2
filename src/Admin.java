488db9c3df9914b0e9a181decac362a36f891557
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-15 22:51:34 +0430

    Complete Admin class add update

--- src/Admin.java
+++ src/Admin.java
@@ -30,9 +30,9 @@ public class Admin {
                     addFlight(input,flights);
                     break;
                 case 2 :
-                    removeFlight(input,flights);
                     break;
                 case 3 :
+                    removeFlight(input,flights);
                     break;
                 case 4:
                     break;
@@ -117,5 +117,95 @@ public class Admin {
                 return i;
         }
         return 0;
+    }
+    private void updateFlight(Flight[] flights , Scanner input)
+    {
+        System.out.println("Enter flightId : ");
+        String flightId = input.next();
+        while (!findExistFlighId(flights,flightId))
+        {
+            System.out.println("This flightId is INCORRECT!");
+            System.out.println("Try again ");
+            System.out.printf("Enter flightId : ");
+            flightId = input.next();
+        }
+        int index = findFlightIdIndex(flights,flightId);
+        switch (adminUpdateMenu(input))
+        {
+            case 1:
+                updateOrigin(flights[index],input);
+                break;
+            case 2:
+                updateDestination(flights[index],input);
+                break;
+            case 3:
+                updateDate(flights[index],input);
+                break;
+            case 4:
+                updateTime(flights[index],input);
+                break;
+            case 5:
+                updatePrice(flights[index],input );
+                break;
+            case 6 :
+                updateSeats(flights[index],input );
+                break;
+        }
+    }
+    private int adminUpdateMenu(Scanner input)
+    {
+        int chose;
+        System.out.println("Select number");
+        System.out.println("< 1 > Origin");
+        System.out.println("< 2 > Destination");
+        System.out.println("< 3 > Date");
+        System.out.println("< 4 > Time");
+        System.out.println("< 5 > Price");
+        System.out.println("< 6 > Seats");
+        System.out.print(">>");
+        chose = input.nextInt();
+        while (chose > 6 || chose<1)
+        {
+            System.out.println("Try again!");
+            System.out.print(">>");
+            chose = input.nextInt();
+        }
+        return chose;
+    }
+    private void updateOrigin(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Origin : %s",flight.getOrigin());
+        System.out.printf("New Origin : ");
+        flight.setOrigin(input.next());
+    }
+    private void updateDestination(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Destination : %s",flight.getDestination());
+        System.out.printf("New Destination : ");
+        flight.setDestination(input.next());
+    }
+    private void updateDate(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Date : %s",flight.getDate());
+        System.out.printf("New Date : ");
+        flight.setDate(input.next());
+    }
+    private void updateTime(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Time : %s",flight.getTime());
+        System.out.printf("New Time : ");
+        flight.setTime(input.next());
+    }
+    private void updatePrice(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Price : %d",flight.getPrice());
+        System.out.printf("New Price : ");
+        flight.setPrice(input.nextInt());
+    }
+    private void updateSeats(Flight flight ,Scanner input)
+    {
+        System.out.printf("Old Seats : %d",flight.getSeats());
+        System.out.printf("New Seats : ");
+        flight.setSeats(input.nextInt());
     }
 }
