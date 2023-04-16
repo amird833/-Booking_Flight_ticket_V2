0ea46c7361b20bb231f4901e24e20b00ca56edf1
Amir Mohammad <adaghaghpoor@gmail.com>
2023-04-16 09:39:41 +0430

    Refactor

--- src/FlightAction.java
+++ src/FlightAction.java
@@ -170,4 +170,122 @@ public class FlightAction {
         flights[2].setPrice(1100000);
         flights[2].setSeats(12);
     }
+    public void filterFlight(Flight[] flights , Scanner input)
+    {
+        System.out.println("Filter by :");
+        switch (searchFlight(input)) {
+            case 1 -> flightIdFilter(flights, input);
+            case 2 -> originFilter(flights, input);
+            case 3 -> destinationFilter(flights, input);
+            case 4 -> dateFilter(flights, input);
+            case 5 -> timeFilter(flights, input);
+            case 6 -> priceFilter(flights, input);
+            case 7 -> seatsFilter(flights, input);
+        }
+    }
+    private int searchFlight(Scanner input)
+    {
+        int chose ;
+        System.out.println("< 1 > FlightId");
+        System.out.println("< 2 > Origin");
+        System.out.println("< 3 > Destination");
+        System.out.println("< 4 > Date");
+        System.out.println("< 5 > Time");
+        System.out.println("< 6 > Price");
+        System.out.println("< 7 > Seats");
+        System.out.print(">>");
+        chose = input.nextInt();
+        while (chose >7 || chose < 1 )
+        {
+            System.out.println("Try again!" );
+            System.out.print(">>");
+            chose=input.nextInt();
+        }
+        return chose;
+    }
+    public void flightIdFilter(Flight[] flights,Scanner input)
+    {
+        String str;
+        System.out.print("Enter FlightId :");
+        str = input.next();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getFlightId().equals(str))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void originFilter(Flight[] flights,Scanner input)
+    {
+        String str;
+        System.out.print("Enter Origin :");
+        str = input.next();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getOrigin().equals(str))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void destinationFilter(Flight[] flights , Scanner input)
+    {
+        String str;
+        System.out.print("Enter Destination :");
+        str = input.next();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getDestination().equals(str))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void dateFilter(Flight[] flights,Scanner input)
+    {
+        String str;
+        System.out.print("Enter Date :");
+        str = input.next();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getDate().equals(str))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void timeFilter(Flight[] flights,Scanner input)
+    {
+        String str;
+        System.out.print("Enter Time :");
+        str = input.next();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getTime().equals(str))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void priceFilter(Flight[] flights,Scanner input)
+    {
+        int price1,price2;
+        System.out.print("Enter Price 1 :");
+        price1 = input.nextInt();
+        System.out.print("Enter Price 2 :");
+        price2 = input.nextInt();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && (flight.getPrice() >= price1 && flight.getPrice()<=price2))
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
+
+    private void seatsFilter(Flight[] flights,Scanner input)
+    {
+        int seats;
+        System.out.print("Enter Seats :");
+        seats = input.nextInt();
+        for (Flight flight : flights) {
+            if (flight!= null && flight.getFlightId() != null && flight.getSeats() == seats)
+                System.out.printf("\t%-10s       | %-10s       | %-10s       | %-10s       | %-10s       | %-,10d       | %-3d\n", flight.getFlightId(), flight.getOrigin(), flight.getDestination(), flight.getDate(), flight.getTime(), flight.getPrice(), flight.getSeats());
+
+        }
+    }
 }
